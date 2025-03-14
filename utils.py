3cb0865c18367f8ff404b83d81895606c9b46dea
ADTranspoResearch <alessandro.dapice@mail.mcgill.ca>
2025-03-14 16:52:22 -0400

    added function to get the loe index, needs to be implemented into choice_set_properties

--- utils.py
+++ utils.py
@@ -8,3 +8,40 @@ def check_dir(path):
         print(f"Directory '{path}' created.")
     else:
         print(f"Directory '{path}' already exists.")
+
+def level_of_effort(slope, length): #given a slope and length of a link, what is the level of effort according to scale from 'Slope stress criteria as a complement to traffic stress criteria, and impact on high comfort bicycle accessibility'
+    loe = 0
+    if length<150:
+        if slope<0.065:
+            return 1
+        elif slope<0.08:
+            return 2
+        elif slope<0.095:
+            return 3
+        elif slope<0.11:
+            return 4
+        else:
+            return 5
+    elif length<500:
+        if slope<0.05:
+            return 1
+        elif slope<0.065:
+            return 2
+        elif slope<0.08:
+            return 3
+        elif slope<0.095:
+            return 4
+        else:
+            return 5
+    else:
+        if slope<0.035:
+            return 1
+        elif slope<0.05:
+            return 2
+        elif slope<0.065:
+            return 3
+        elif slope<0.08:
+            return 4
+        else:
+            return 5
+ 
