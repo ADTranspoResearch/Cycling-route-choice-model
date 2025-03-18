7a094ad1f6f4584bd710bf3549cb6c8cb7990589
ADTranspoResearch <alessandro.dapice@mail.mcgill.ca>
2025-03-18 11:37:41 -0400

    added LOE and LTS related variables to choice set properties

--- choice_set_properties.py
+++ choice_set_properties.py
@@ -27,6 +27,7 @@ def get_path_properties(edge_path_list, chosen_state=0, ini_len=0):
     Q85_list = []
     Q85_weight_list = []
     loe_index_list = []
+    loe_index_w_list = []
     not_included_links = 0
     path_size = 0
     if ini_len!=0:
@@ -65,7 +66,9 @@ def get_path_properties(edge_path_list, chosen_state=0, ini_len=0):
                 Q85_list.append(Q85)
                 Q85_weight_list.append(Q85*edge_length)
             if slope and edge_length:
-                loe_index_list.append(level_of_effort(slope, edge_length))
+                loe_index = level_of_effort(slope, edge_length)
+                loe_index_list.append(loe_index)
+                loe_index_w_list.append(loe_index*edge_length)
         else:
             lts = 0
 
@@ -80,14 +83,15 @@ def get_path_properties(edge_path_list, chosen_state=0, ini_len=0):
 #breaking up the dictionary into multiple dicts to make it more readable
     #dictionary for anything related to LTS
     lts_dict = {'avg_lts':(sum(lts_list)/max(len(lts_list),1)), 'max_lts' : max(lts_list, default=0), 'avg_lts_dist_w' : (sum(lts_weight_list)/max(len(lts_weight_list),1)),
-                'lts_sum':sum(lts_list)}
+                'lts_sum':sum(lts_list), 'lts_dist_w_sum':sum(lts_weight_list)}
     #dictionary for any raw road attributes
-    att_dict = {'length':path_length,'number of links' : path_size, 'infra_length' : cycl_length, 'infra_ratio' : min((cycl_length/max(path_length,1)),1), 
+    att_dict = {'length':path_length,'number_of_links' : path_size, 'infra_length' : cycl_length, 'infra_ratio' : min((cycl_length/max(path_length,1)),1), 
                 'chosen':chosen_state,'not_included_links' : not_included_links}
     
     #dictionary for any attributes related to slope or level of effort
     loe_dict = {'avg_slope':(sum(slope_list)/max(len(slope_list),1)), 'max_slope' : max(slope_list, default=0), 'avg_loe':(sum(loe_list)/max(len(loe_list),1)),
-             'max_loe' : max(loe_list, default=0), 'loe_index_sum':sum(loe_index_list) }
+             'max_loe' : max(loe_list, default=0), 'loe_index_sum':sum(loe_index_list),'loe_dist_w_sum':sum(loe_index_w_list), 
+             'loe_index_avg_dist_w':sum(loe_index_w_list)/max(len(loe_index_w_list),1)}
     
     #dictionary for any attributes about the vehicle volume or usage on links
     trf_dict = {'avg_ADT' : (sum(adt_list)/max(len(adt_list),1)), 'avg_adt_dist_w' : (sum(adt_weight_list)/max(len(adt_weight_list),1)), 
