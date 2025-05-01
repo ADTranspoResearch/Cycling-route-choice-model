f7bf310ae95d66fffccb4d7efd6133a176c1bd60
ADTranspoResearch <alessandro.dapice@mail.mcgill.ca>
2025-05-01 10:15:15 -0400

    added linting, added check to ignore cyclists where chosen path contained unknown links

--- choice_set_properties.py
+++ choice_set_properties.py
@@ -10,10 +10,14 @@ import pandas as pd
 from utils import check_dir, level_of_effort
 
 
-def cyclist_error(cyc_id):
+def cyclist_error(cyc_id, reason=0):
     """prints problematic cyclist id and appends to error_id dict"""
-    error_id[cyc_id] = 'error'
-    print(f"warning: cyclist {cyc_id} did not create a properties file!")
+    if reason == 0:
+        print(f"warning: cyclist {cyc_id} did not create a properties file!")
+        error_id[cyc_id] = 'error'
+    elif reason == 1:
+        print(f"cyclist {cyc_id} had a unknown link in chosen path!")
+        error_id[cyc_id] = 'path_size-error'
 
 
 
@@ -32,11 +36,11 @@ def get_path_properties(cyc_path, chosen_state=0, ini_len=0):
     loe_index_list = []
     loe_index_w_list = []
     not_included_links = 0
-    ps = 0
+    num_links = 0
     if ini_len!=0:
         path_length= int(ini_len)
     for edge in cyc_path:
-        ps+=1
+        num_links+=1
         if int(edge) == 1607918:
             pass
         if int(edge) not in db_df.index:
@@ -105,7 +109,7 @@ def get_path_properties(cyc_path, chosen_state=0, ini_len=0):
     # dictionary for any raw road attributes
     att_dict = {
         'length':path_length,
-        'number_of_links' : ps,
+        'number_of_links' : num_links,
         'infra_length' : cycl_length,
         'infra_ratio' : min((cycl_length/max(path_length,1)),1),
         'chosen':chosen_state,'not_included_links' : not_included_links,
@@ -178,158 +182,170 @@ late_start = False
 # Files are not sorted in numerical order, for example 3203 is index,
 # 3080 but 52 is later, if using late start,
 # 3203 will be done but not 52
-late_start_by_index = True
+late_start_by_index = False
 start_id = 28942
 start_index = 3175
 index_count = 0
+debug_break_outer = False
+
 for choice_set_file in trip_list:
-    try:
-        idnum = int(choice_set_file.split("_")[0])
-        if late_start:
-            if idnum<start_id:
-                continue
-        if late_start_by_index:
-            if index_count<start_index:
-                index_count+=1
-                continue
-
-        if debug:
-            idnum =debug_id
-        if idnum not in trip_id_list:
-            print(idnum,"not in trip list, skipping...")
+    idnum = int(choice_set_file.split("_")[0])
+    # this section of code checks all the debugging options
+    if late_start:
+        if idnum<start_id:
             continue
-        choice_id=0
-        path_dict = {}
-        path_size_dict={}
-        edge_path_list=[]
-        shortest_path_list=[]
-        print('cyclist:',idnum)
-        with open(
+    if late_start_by_index:
+        if index_count<start_index:
+            index_count+=1
+            continue
+    if debug:
+        idnum =debug_id
+
+    # start of actual loop
+    if idnum not in trip_id_list:
+        print(idnum,"not in trip list, skipping...")
+        continue
+
+    # Initialize all required empty lists
+    choice_id = 0
+    path_dict = {} # key=alt path id: value=properties of alternative
+    path_size_dict = {} # key=alt path id: value=list of edges in path
+    shortest_path_list = [] #contains length of all paths in choice set
+    print('cyclist:',idnum)
+
+    with open(
             f"{choice_set_filepath}{idnum}_choice_set.json", 'r') as json_file:
-            choice_set = json.load(json_file)
+        choice_set = json.load(json_file)
 
 
-        chosen_path = ast.literal_eval(
-            chosen_path_df['trip_segment'].loc[idnum])
+    chosen_path = ast.literal_eval(
+        chosen_path_df['trip_segment'].loc[idnum])
 
 
-        path_dict[choice_id] = get_path_properties(
-            chosen_path, 1, ini_len=chosen_path_df['length'].loc[idnum])
-        path_size_dict[choice_id]=chosen_path
+    path_dict[choice_id] = get_path_properties(
+        chosen_path, 1, ini_len=chosen_path_df['length'].loc[idnum])
+    path_size_dict[choice_id]=chosen_path
+    shortest_path_list.append(path_dict[choice_id]['length'])
+    choice_id+=1
+
+
+    # To get path size, change loop to create a dictionary with
+    # key:choice_id value:edge_path_list and then loop through
+    # creating PS factor and THEN run get_properties_function
+    for path in choice_set:
+        edge_path_list=[]
+        # Appends all the edges used in the path to edge_path_list
+        # and assigns this list to path_size dict with alt_id as key
+        for i in range(0,(len(path)-1)):
+            j=i+1
+
+            edge_path_list.append(G.edges[path[i],path[j]]['ID_RD'])
+        path_size_dict[choice_id] = edge_path_list
+        path_dict[choice_id] = get_path_properties(edge_path_list, 0)
         shortest_path_list.append(path_dict[choice_id]['length'])
         choice_id+=1
 
-        #get edge id list
-
-        # To get path size, change loop to create a dictionary with
-        # key:choice_id value:edge_path_list and then loop through
-        # creating PS factor and THEN run get_properties_function
-        for path in choice_set:
-            edge_path_list=[]
-
-            for i in range(0,(len(path)-1)):
-                j=i+1
-
-                edge_path_list.append(G.edges[path[i],path[j]]['ID_RD'])
-            path_size_dict[choice_id] = edge_path_list
-            path_dict[choice_id] = get_path_properties(edge_path_list, 0)
-            shortest_path_list.append(path_dict[choice_id]['length'])
-            choice_id+=1
-
-
-        shortest_path_len= min(shortest_path_list)
-        for choice_id,path_list in path_size_dict.items():
-            path_len=path_dict[choice_id]['length']
-            path_size=0
-            for link in path_list:
-                try:
-                    link_len = pd.Series(
-                        db_df['length'].loc[int(link)]).iloc[0]
-                    #TODO: investigate why so many paths have unknown
-                    # links, possibly use network to find replacement
-                except KeyError:
-                    # If the length of the link is unknown,
-                    # assign it the average link length
-                    link_len = db_df['length'].mean()
-
-
-                first_term = link_len/path_len
-                denomonator = 0
-                for alt_id, alt_path in path_size_dict.items():
-                    if link in alt_path:
-
-                        denomonator += (
-                            shortest_path_len
-                            /path_dict[alt_id]['length']
-                        )
-                path_size+= first_term/denomonator
-            path_dict[choice_id]['path_size']=path_size
-
-        all_columns = set()
-        for value_dict in path_dict.values():
-            all_columns.update(value_dict.keys())
-
-
-        # getting demographic values
-        # NOT USABLE UNTIL ID MATCHING METHOD IS RESOLVED
-        if ADD_DEMOGRAPHICS:
+    # section for getting the path size factor
+    shortest_path_len= min(shortest_path_list)
+    for choice_id,path_list in path_size_dict.items():
+        unknown_count = 0 # debug, delete if not in use
+        path_len=path_dict[choice_id]['length']
+        path_size=0
+        for link in path_list:
             try:
-                gender = demo_df['gender'].iloc[int(idnum)]
-            except IndexError:
-                gender = 0
-            try:
-                age = demo_df['age'].iloc[int(idnum)]
-            except IndexError:
-                age = 0
-            try:
-                income = demo_df['income'].iloc[int(idnum)]
-            except IndexError:
-                income = 0
+                link_len = pd.Series(
+                    db_df['length'].loc[int(link)]).iloc[0]
+                #TODO: investigate why so many paths have unknown
+                # links, possibly use network to find replacement
+            except KeyError:
+                # If the length of the link is unknown,
+                # assign it the average link length
+                link_len = db_df['length'].mean()
+                unknown_count+=1
+                debug_break_outer = True
+                break
 
 
-        # Add 'choice_id' as the first column
-        if ADD_DEMOGRAPHICS:
-            csv_columns = (
-                ['choice_id']
-                + sorted(all_columns)
-                + ['gender', 'age','income']
-            )
-        else:
-            csv_columns = ['choice_id'] + sorted(all_columns)
-        # Open the CSV file for writing
-        output_file = output_filepath+'properties_'+str(idnum)+'.csv'
-        with open(output_file, 'w', newline='',) as csvfile:
-            # Create a CSV DictWriter object
-            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            first_term = link_len/path_len
+            denomonator = 0
+            for alt_id, alt_path in path_size_dict.items():
+                if link in alt_path:
 
-            # Write the CSV header
-            writer.writeheader()
-
-            # Write each entry in path_dict as a row
-            for key, value_dict in path_dict.items():
-                # Include the 'choice_id' in the row data
-                if ADD_DEMOGRAPHICS:
-                    row = {
-                        'choice_id': key,
-                        'gender': gender,
-                        'age' : age,
-                        'income' : income
-                        }
-                else:
-                    row = {'choice_id': key}
-                # Add the rest of the columns from the value dict
-                row.update(value_dict)
-
-                # Write the row to the CSV
-                writer.writerow(row)
-
-        print(f"Successfully written to CSV: {output_filepath}_{idnum}")
-        if debug:
+                    denomonator += (
+                        shortest_path_len
+                        /path_dict[alt_id]['length']
+                    )
+            path_size+= first_term/denomonator
+        if debug_break_outer:
             break
-    except RecursionError:
-        cyclist_error(idnum)
-        if debug:
+        path_dict[choice_id]['path_size']=path_size
+        if debug_break_outer:
             break
+    if debug_break_outer:
+        debug_break_outer = False
+        cyclist_error(idnum,reason=1)
+        continue
+    all_columns = set()
+    for value_dict in path_dict.values():
+        all_columns.update(value_dict.keys())
+
+
+    # getting demographic values
+    # NOT USABLE UNTIL ID MATCHING METHOD IS RESOLVED
+    if ADD_DEMOGRAPHICS:
+        try:
+            gender = demo_df['gender'].iloc[int(idnum)]
+        except IndexError:
+            gender = 0
+        try:
+            age = demo_df['age'].iloc[int(idnum)]
+        except IndexError:
+            age = 0
+        try:
+            income = demo_df['income'].iloc[int(idnum)]
+        except IndexError:
+            income = 0
+
+
+    # Add 'choice_id' as the first column
+    if ADD_DEMOGRAPHICS:
+        csv_columns = (
+            ['choice_id']
+            + sorted(all_columns)
+            + ['gender', 'age','income']
+        )
+    else:
+        csv_columns = ['choice_id'] + sorted(all_columns)
+    # Open the CSV file for writing
+    output_file = output_filepath+'properties_'+str(idnum)+'.csv'
+    with open(output_file, 'w', newline='',) as csvfile:
+        # Create a CSV DictWriter object
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+
+        # Write the CSV header
+        writer.writeheader()
+
+        # Write each entry in path_dict as a row
+        for key, value_dict in path_dict.items():
+            # Include the 'choice_id' in the row data
+            if ADD_DEMOGRAPHICS:
+                row = {
+                    'choice_id': key,
+                    'gender': gender,
+                    'age' : age,
+                    'income' : income
+                    }
+            else:
+                row = {'choice_id': key}
+            # Add the rest of the columns from the value dict
+            row.update(value_dict)
+
+            # Write the row to the CSV
+            writer.writerow(row)
+
+    print(f"Successfully written to CSV: {output_filepath}_{idnum}")
+    if debug:
+        break
 
 
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -343,6 +359,6 @@ with open(
     writer.writerow(['cyclist_id', 'reason'])
 
     # Write each key-value pair as a row
-    for cyclist_id, reason in error_id.items():
-        writer.writerow([cyclist_id, reason])
+    for cyclist_id, err_reason in error_id.items():
+        writer.writerow([cyclist_id, err_reason])
 print(f"{len(error_id)} cyclist trips had issues")
